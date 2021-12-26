ca17f3e387d3db97629d973017f85c570aa27208
poojanidoni <poojanidonixworksz@gmail.com>
2021-12-26 22:18:09 +0530

    hi

---
+++ Car.java
@@ -0,0 +1,9 @@
+class Car{
+	public static void main(String []demo){
+		System.out.println("main() started");
+	for(int index=0; index<riya.length; index++)
+		System.out.println(riya[index]);
+	}
+		System.out.println("End of the main()");
+	}
+}
